6a1e2f3af0b4b2142ae5658f2c0e7676cf0d872f
HelloWorld <whitesail2008@gmail.com>
2023-03-03 19:51:45 -0500

    add the new files

---
+++ src/main/java/com/whitesail/aero/testrepo/TestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.whitesail.aero.testrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whitesail/aero/testrepo/TestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitesail.aero.testrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
